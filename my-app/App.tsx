dadda594da5bc3ee23743ecc2a2ca329d0105a81
ronaldophc <ronaldohortmann02@gmail.com>
2024-10-13 16:53:55 -0300

    exercise done

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from'expo-image';
 
 export default function App() {
+
+  const pet = {
+    animal: 'Cat',
+    name: "Buddy",
+    age: 21
+  };
+  if (pet.animal == 'Dog') {
+    return (
+      <View style={styles.container}>
+        <Text>Who let the dogs out: {pet.name}, {pet.age}</Text>
+        <Image source={{ uri: 'https://picsum.photos/200/300' }} style={{ width: 150, height: 150 }} />
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Joao Bacelar maior viado da terra!</Text>
-      <StatusBar style="auto" />
+      <Text>Are we in Jumanji {pet.animal} {pet.name}?</Text>
+  
     </View>
   );
 }
